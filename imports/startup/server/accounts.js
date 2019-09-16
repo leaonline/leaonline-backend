b2b2de7fa865e621bcbba797a2ca467e8fdaf455
Jan Kuester <jkuester@uni-bremen.de>
2019-09-16 14:00:56 +0200

    first basic project start

---
+++ imports/startup/server/accounts.js
@@ -0,0 +1,11 @@
+import { Meteor } from 'meteor/meteor'
+import { Accounts } from 'meteor/accounts-base'
+
+if (Meteor.isDevelopment) {
+  Meteor.startup(() => {
+    if (Meteor.users.find().count() === 0) {
+      const userId = Accounts.createUser({ username: 'admin', password: 'password' })
+      console.info(`Devmode: user created with id ${userId} - login via admin / password`)
+    }
+  })
+}
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+meteor --port=5050 --settings=settings.json
