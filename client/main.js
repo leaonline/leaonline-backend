4656be1bb1d3b78ceffbd12411328abb494b4007
Jan Küster <jkuester@uni-bremen.de>
2019-09-28 12:07:55 +0200

    added bootstrap and fontawesome and scss compiler pipeline

--- client/main.js
+++ client/main.js
@@ -1,4 +1,6 @@
 import '../imports/startup/client/language'
 import '../imports/startup/client/routes'
+import '../imports/startup/client/fontawesome'
+import '../imports/startup/client/bootstrap'
 import './main.html'
 
---
+++ imports/startup/client/bootstrap.js
@@ -0,0 +1,5 @@
+import 'bootstrap'
+import 'bootstrap/scss/bootstrap.scss'
+import popper from 'popper.js'
+
+global.Popper = global.Popper || popper
