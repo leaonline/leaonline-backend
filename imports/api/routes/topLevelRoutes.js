cb73eea3c9526eadf1d004a6f424b91e84e4080c
Jan Kuester <jkuester@uni-bremen.de>
2019-09-30 16:10:40 +0200

    api RputesTree aded to represent subroute structure for sidebar

--- imports/api/routes/topLevelRoutes.js
+++ imports/api/routes/topLevelRoutes.js
@@ -1,6 +1,21 @@
-import { Routes } from './Routes'
+export const RoutesTree = {}
 
-export const topLevelRoutes = [
-  Routes.dashboard,
-  Routes.statusOverview
-]
+const _routes = {}
+
+RoutesTree.topLevel = function (name, route) {
+  _routes[ name ] = route
+}
+
+RoutesTree.children = function (parentName, route, data = []) {
+  _routes[ parentName ].children = data.map(entry => {
+    const href = route.path(...entry.args)
+    return {
+      label: entry.label,
+      href: href
+    }
+  })
+}
+
+RoutesTree.get = function () {
+  return Object.values(_routes)
+}
