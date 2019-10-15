78fab015a954c1a371570375738a25c2a08f6896
Jan Kuester <jkuester@uni-bremen.de>
2019-10-15 13:57:02 +0200

    scripts run.sh updated METEOR_PACKAGE_DIRS

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-METEOR_PACKAGE_DIRS=../lib meteor --port=5050 --settings=settings.json
+meteor npm install
+
+PACKAGE_DIRS="../lib:../liboauth"
+METEOR_PACKAGE_DIRS=${PACKAGE_DIRS}  meteor --port=5050 --settings=settings.json
