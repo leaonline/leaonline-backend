2128fee38dd9393f81e9f9a05d449f3777939bf5
jankapunkt <jkuester@uni-bremen.de>
2019-10-27 19:47:49 +0100

    pass tranlsator from i18n to LeaCoreLib context

--- imports/startup/client/language.js
+++ imports/startup/client/language.js
@@ -1,4 +1,5 @@
 import { i18n } from '../../api/i18n/I18n'
+import { LeaCoreLib } from '../../api/core/LeaCoreLib'
 import i18nConfig from '../../../resources/i18n/i18n_config'
 import i18nDE from '../../../resources/i18n/i18n_de' // TODO load from backend
 
@@ -7,7 +8,8 @@ const config = {
   de: i18nDE
 }
 
-i18n.load(config)
+const translator = i18n.load(config)
+LeaCoreLib.i18n.load(translator)
 
 Meteor.startup(() => {
   document.documentElement.setAttribute('lang', 'de')
