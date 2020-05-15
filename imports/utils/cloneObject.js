207feefaeb54371877d242f104adae3370d4f624
Jan Küster <jkuester@uni-bremen.de>
2020-05-15 09:51:59 +0200

    added utils cloneObject#

---
+++ imports/utils/cloneObject.js
@@ -0,0 +1,13 @@
+
+export const cloneObject = (original) => {
+  const copy = {}
+  Object.entries(original).forEach(([key, value]) => {
+    Object.defineProperty(copy, key, {
+      value: Object.assign({}, value),
+      writable: true,
+      enumerable: true
+    })
+  })
+
+  return copy
+}
