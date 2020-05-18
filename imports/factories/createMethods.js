e1c1c58cf8ce99b44f8a211ed8a1c5e97873d130
Jan Küster <jkuester@uni-bremen.de>
2020-05-18 11:15:48 +0200

    api method factory added permission mixin

--- imports/factories/createMethods.js
+++ imports/factories/createMethods.js
@@ -1,8 +1,10 @@
 import { createMethodFactory } from 'meteor/leaonline:method-factory'
 import { Schema } from '../api/schema/Schema'
+import { checkPermissions } from '../api/mixins/checkPermissions'
 
 export const createMethod = createMethodFactory({
-  schemaFactory: Schema.create
+  schemaFactory: Schema.create,
+  mixins: [checkPermissions]
 })
 
 export const createMethods = methods => methods.forEach(createMethod)
