c2748814aedc0fb997ceb758612a3abb82ecf3a2
Jan Küster <jkuester@uni-bremen.de>
2020-05-18 09:39:55 +0200

    ui nav overflow fixed

--- imports/ui/layout/nav/side/navSide.js
+++ imports/ui/layout/nav/side/navSide.js
@@ -1,5 +1,6 @@
 import { Template } from 'meteor/templating'
 import { RoutesTree } from '../../../../api/routes/topLevelRoutes'
+import './navSide.scss'
 import './navSide.html'
 
 Template.navSide.helpers({
