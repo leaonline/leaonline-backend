64f363a4ed1480eaed9808d743fa4ded95bf8492
Jan Küster <jkuester@uni-bremen.de>
2020-06-15 12:26:08 +0200

    api Routes topLevelRoutes.get return entry by name

--- imports/api/routes/topLevelRoutes.js
+++ imports/api/routes/topLevelRoutes.js
@@ -3,20 +3,23 @@ import { ReactiveDict } from 'meteor/reactive-dict'
 export const RoutesTree = {}
 
 const _routes = new ReactiveDict()
-const _map = {}
+const _map = new Map()
 
 RoutesTree.topLevel = function (name, route) {
   _routes.set(name, JSON.stringify(route))
-  _map[name] = route
+  _map.set(name, route)
 }
 
 RoutesTree.children = function (parentName, route, data = []) {
   if (!_routes.get(parentName)) return
-  const parentRoute = _map[parentName]
+  const parentRoute = _map.get(parentName)
   parentRoute.children = data
   _routes.set(parentName, JSON.stringify(route))
 }
 
-RoutesTree.get = function () {
-  return Object.keys(_routes.all()).map(key => _map[key])
+RoutesTree.get = function (name) {
+  if (name) {
+    return _map.get(name)
+  }
+  return Object.keys(_routes.all()).map(key => _map.get(key))
 }
