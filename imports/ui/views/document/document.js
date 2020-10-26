7e2e1d0da34ee40a55bc3f4a2df57640191a96da
Jan Küster <jkuester@uni-bremen.de>
2020-10-26 11:52:20 +0100

    ui views document improved

--- imports/ui/views/document/document.js
+++ imports/ui/views/document/document.js
@@ -1,11 +1,23 @@
 import { Template } from 'meteor/templating'
-import { StateVariables, wrapHelpers, wrapOnCreated } from '../../config/backendConfigWrappers'
-import { formIsValid } from '../../../utils/form'
+import { wrapHelpers, wrapOnCreated } from '../../config/backendConfigWrappers'
+import { formIsValid, validateFormData } from '../../../utils/form'
+import { StateVariables } from '../../config/StateVariables'
+import '../../components/stringified/stringified'
 import './document.html'
+import { Router } from '../../../api/routes/Router'
+import { by300 } from '../../../utils/dely'
+import { defineUndefinedFields } from '../../../utils/defineUndefinedFields'
+import { defaultNotifications } from '../../../utils/defaultNotifications'
+
+const readonly = ''
 
 Template.genericDocument.onCreated(function () {
   const instance = this
-  const onSubscribed = () => instance.state.set('updateDoc', instance.mainCollection.findOne() || {})
+  const onSubscribed = () => {
+    const updateDoc = instance.mainCollection.findOne() || {}
+    delete updateDoc._id
+    instance.state.set({ updateDoc })
+  }
 
   instance.autorun(() => {
     const data = Template.currentData()
@@ -29,29 +41,61 @@ Template.genericDocument.helpers(wrapHelpers({
   },
   isUpdating () {
     return Template.instance().state.get('isUpdating')
+  },
+  editMode () {
+    return Template.instance().state.get('editMode')
   }
 }))
 
 Template.genericDocument.events({
+  'click .edit-button' (event, templateInstance) {
+    event.preventDefault()
+    templateInstance.state.set('editMode', true)
+  },
+  'click .cancel-edit-button' (event, templateInstance) {
+    event.preventDefault()
+    templateInstance.state.set('editMode', false)
+  },
   'submit #updateForm' (event, templateInstance) {
     event.preventDefault()
+    const updateDoc = formIsValid('updateForm', templateInstance.actionUpdateSchema, {
+      template: templateInstance,
+      fallback: false
+    })
 
-    const app = templateInstance.data.app()
-    const { connection } = app
-    const actionUpdate = templateInstance.state.get(StateVariables.actionUpdate)
-    const updateDoc = templateInstance.state.get('updateDoc')
-    const insertDoc = formIsValid('updateForm', templateInstance.actionUpdateSchema)
-    if (!insertDoc) {
-      return
+    if (!updateDoc) return
+
+    const target = templateInstance.state.get('updateDoc')
+    updateDoc._id = target._id
+
+    // we need to clean both documents in case the schema is updated
+    // and the target still contains fields that are not part of the schema
+    const cleanOptions = {
+      mutate: true,
+      filter: true,
+      autoConvert: true,
+      removeEmptyStrings: true
     }
 
+    templateInstance.actionUpdateSchema.clean(updateDoc, cleanOptions)
+    templateInstance.actionUpdateSchema.clean(target, cleanOptions)
+
+    defineUndefinedFields(updateDoc, target)
+
+    templateInstance.state.set(StateVariables.submitting, true)
     templateInstance.state.set('isUpdating', true)
-    const finalDoc = Object.assign({}, insertDoc, { _id: updateDoc._id })
-    connection.call(actionUpdate.method, finalDoc, (err, res) => {
-      setTimeout(() => {
-        templateInstance.state.set('isUpdating', false)
-      }, 500)
-      console.log(err, res)
-    })
+    const actonUpdate = templateInstance.state.get('actionUpdate')
+    const app = templateInstance.data.app()
+    const { connection } = app
+    connection.call(actonUpdate.name, updateDoc, by300((err, res) => {
+      templateInstance.state.set(StateVariables.submitting, false)
+      defaultNotifications(err, res)
+        .success(function () {
+          Router.queryParam({ action: null })
+          templateInstance.state.set('updateForm', false)
+          templateInstance.state.set('isUpdating', false)
+          templateInstance.state.set('editMode', false)
+        })
+    }))
   }
 })
