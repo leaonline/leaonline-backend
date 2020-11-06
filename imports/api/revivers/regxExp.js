3ae4d871e12a16a6058303ce8133ed1c0059b718
Jan Küster <jkuester@uni-bremen.de>
2020-11-06 12:28:24 +0100

    js lint fix

--- imports/api/revivers/regxExp.js
+++
@@ -1,7 +0,0 @@
-import { EJSON } from 'meteor/ejson'
-
-export const isRegExp = (key, value) => {
-  if (value && value["$regexp"]) {
-    return EJSON.fromJSONValue(value)
-  }
-}
--- imports/startup/client/schemaConfigReviver.js
+++ imports/startup/client/schemaConfigReviver.js
@@ -2,7 +2,7 @@ import { JSONReviver } from '../../api/config/JSONReviver'
 import { isLabel } from '../../api/revivers/isLabel'
 import { isOptions } from '../../api/revivers/isOptions'
 import { isFirstOptions } from '../../api/revivers/isFirstOption'
-import { isRegExp } from '../../api/revivers/regxExp'
+import { isRegExp } from '../../api/revivers/isRegxExp'
 import { isPrimitiveConstructor } from '../../api/revivers/isPrimitiveConstructor'
 
 JSONReviver.register(isLabel)
