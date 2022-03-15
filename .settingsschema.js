f9e1a9facbd664b1c6bf72b042542d42ba04e479
Jan Küster <jkuester@uni-bremen.de>
2022-03-15 08:31:17 +0100

    feature: validate settings schema

---
+++ .settingsschema.js
@@ -0,0 +1,54 @@
+const SimpleSchema = require('simpl-schema')
+const oauthFlows = ['authorization_code']
+const schema = def => new SimpleSchema(def)
+
+const urlField = {
+  type: String,
+  regEx: SimpleSchema.RegEx.url
+}
+
+const remoteApp = {
+  label: String,
+  name: String,
+  url:  urlField,
+  icon: String,
+  ddpConnect: Boolean,
+  ddpLogin: Boolean
+}
+
+const settingsSchema = schema({
+  oauth: schema({
+    clientId: String,
+    secret: String,
+    dialogUrl:  urlField,
+    accessTokenUrl:  urlField,
+    authorizeUrl:  urlField,
+    identityUrl:  urlField,
+    redirectUrl:  urlField
+  }),
+  public: schema({
+    app: schema({
+      title: String
+    }),
+    editor: schema({
+      textAreaThreshold: SimpleSchema.Integer
+    }),
+    hosts: schema({
+      otulea: schema(remoteApp),
+      content: schema(remoteApp),
+      teacher: schema(remoteApp)
+    }),
+    tts: schema({
+      url: urlField
+    }),
+    pages: schema({
+      sessions: schema({
+        use: String
+      })
+    })
+  })
+})
+
+module.exports = function (settings) {
+  settingsSchema.validate(settings)
+}
